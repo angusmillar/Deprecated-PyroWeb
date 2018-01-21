dd4de985de33a5185e7e68b9e060d41c22621562
Angus Millar <angusmillar@iinet.net.au>
2018-01-21 16:05:06 +0800

    Added commented out testing variable

--- src/Constants/FhirServerConstant.js
+++ src/Constants/FhirServerConstant.js
@@ -4,6 +4,9 @@ export default class FhirServerConstant {
     //GG's Server    
     //static PrimaryFhirServerEndpoint = 'http://test.fhir.org/r3'
     
+    //Testing
+    // static PrimaryFhirServerEndpoint = 'http://nowherex'
+
     // PyroHealth
     static PrimaryFhirServerEndpoint = 'https://pyrohealth.net/test/stu3/fhir'
     static RequestTimeout = 40000;
