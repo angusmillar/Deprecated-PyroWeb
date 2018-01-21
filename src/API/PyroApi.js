b3d9a06a8a317be2e2e2ae9aeec16ae4f8bc7cdb
Angus Millar <angusmillar@iinet.net.au>
2018-01-21 11:31:47 +0800

    Added FHIR Server URL to error message on timeout from API request

--- src/API/PyroApi.js
+++ src/API/PyroApi.js
@@ -49,7 +49,12 @@ class PyroApi {
     getMetaData() {
         axios.get('metadata', this.RequestConfig)
             .then(function (response) {
-                const OutCome = new AjaxOutcome(response.status, AjaxConstants.CallCompletedState.Completed_Ok, response.data, null);
+                //Sucessful call return the data
+                const OutCome = new AjaxOutcome(
+                    response.status,
+                    AjaxConstants.CallCompletedState.Completed_Ok,
+                    response.data,
+                    null);
                 AppActionsMetadata.setMetadata(OutCome);
             })
             .catch(function (error) {
@@ -57,18 +62,30 @@ class PyroApi {
                     // The request was made and the server responded with a status code
                     // that falls out of the range of 2xx
                     //FHIR OperationOutcome should be recived
-                    const OutCome = new AjaxOutcome(error.response.status, AjaxConstants.CallCompletedState.Completed_ResponseNotOk, error.response.data, 'HTTP Status retured was: ${error.response.status}!:${error.response.statusText}!');                    
+                    const OutCome = new AjaxOutcome(
+                        error.response.status,
+                        AjaxConstants.CallCompletedState.Completed_ResponseNotOk,
+                        error.response.data,
+                        'HTTP Status retured was: ${error.response.status}!:${error.response.statusText}!');                    
                     AppActionsMetadata.setMetadata(OutCome);
                 } else if (error.request) {
                     // The request was made but no response was received
                     // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
                     // http.ClientRequest in node.js
                     // const OutCome = new AjaxOutcome(null, AjaxConstants.CallCompletedState.Completed_NoResponse, null, `The request was made but no response was received after ${this.RequestConfig.timeout / 1000} secs`);
-                    const OutCome = new AjaxOutcome(null, AjaxConstants.CallCompletedState.Completed_NoResponse, null, `The request was made but no response was received after ${FhirServerConstant.RequestTimeout.toString()} secs`);
+                    const OutCome = new AjaxOutcome(
+                        null,
+                        AjaxConstants.CallCompletedState.Completed_NoResponse,
+                        null, `
+                        The request was made to the server ${FhirServerConstant.PrimaryFhirServerEndpoint} yet no response was received after ${FhirServerConstant.RequestTimeout.toString()} secs`);
                     AppActionsMetadata.setMetadata(OutCome);
                 } else {
                     // Something happened in setting up the request that triggered an Error
-                    const OutCome = new AjaxOutcome(null, AjaxConstants.CallCompletedState.Completed_CallSetupFailed, null, 'Something happened in setting up the request that triggered an Error. Message: ${error.message}!');
+                    const OutCome = new AjaxOutcome(
+                        null,
+                        AjaxConstants.CallCompletedState.Completed_CallSetupFailed,
+                        null,
+                        'Something happened in setting up the request that triggered an Error. Message: ${error.message}!');
                     AppActionsMetadata.setMetadata(OutCome);
                 }
             }
