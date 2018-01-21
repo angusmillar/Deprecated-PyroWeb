d968d0a2b905aa47040d5ed93c23786abbc4201a
Angus Millar <angusmillar@iinet.net.au>
2018-01-21 16:05:32 +0800

    Added Gone and Not Found Http statuses

--- src/Constants/HttpConstant.js
+++ src/Constants/HttpConstant.js
@@ -7,7 +7,10 @@ export default class HttpConstant {
     static StatusCodeArray = [
         { number: '200', description: 'Ok', color: 'olive' },
         { number: '400', description: 'Bad Request', color: 'red' },        
+        { number: '410', description: 'Gone', color: 'orange' },   
+        { number: '404', description: 'Not Found', color: 'orange' },   
     ];
+    
 
     static getStatusCodeByNumber(number) {
         return find(HttpConstant.StatusCodeArray, ['number', number])
