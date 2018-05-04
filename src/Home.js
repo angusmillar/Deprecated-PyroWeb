b0603f45fa8831bde549a620547bcd90e641d514
Angus Millar <angusmillar@iinet.net.au>
2018-05-04 17:08:39 +0800

    Changed sentence to remove the word passion used twice, comma after name

--- src/Home.js
+++ src/Home.js
@@ -93,8 +93,8 @@ export default class Home extends React.Component {
                                     Who am I
                                 </Header.Content>
                             </Header>
-                            <p><WebLink newTab={true} url={'https://www.linkedin.com/in/angus-millar-64298342/'} display={'Angus Millar'} /> I&#39;m an Australian integration specialist with a passion for HL7 integration and informatics.
-                            My passion grew from many years working in pathology laboratories later moving into Laboratory Information Systems (LIS) support and development at <WebLink newTab={true} url={'http://www.kestral.com.au/'} display={'Kestral Computing'} />.
+                            <p><WebLink newTab={true} url={'https://www.linkedin.com/in/angus-millar-64298342/'} display={'Angus Millar'} />, I&#39;m an Australian integration specialist with a passion for HL7 integration and informatics.
+                            This grew from many years working in pathology laboratories later moving into Laboratory Information Systems (LIS) support and development at <WebLink newTab={true} url={'http://www.kestral.com.au/'} display={'Kestral Computing'} />.
                             More recently I have worked on EMR integration at QLD Health and currently, work as a Solution Architect at the <WebLink newTab={true} url={'https://www.digitalhealth.gov.au/'} display={'Australian Digital Health Agency'} /> (formally known as NeHTA).
                             I regularly attend the Australian FHIR Connectathons and run the beginner streams helping educate newcomers to the FHIR specification.</p>
                         </Segment>
